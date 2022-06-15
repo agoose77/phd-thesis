a64355214228fabaadf11d347d899b0b8e7959a0
Angus Hollands <goosey15@gmail.com>
2022-06-15 19:21:42 +0100

    Feat: work on rendering graphs inline
    Former-commit-id: e6f5b2574a7f20bfb0443a9de319f4cf67e91e50

--- ext/policy.py
+++ ext/policy.py
@@ -6,5 +6,5 @@ def setup_assets_policy(app, config):
 def setup(app):
     app.add_config_value("html_assets_policy", "per_page", "html")
     app.connect('config-inited', setup_assets_policy)
-    
+
     print(app.builder)
